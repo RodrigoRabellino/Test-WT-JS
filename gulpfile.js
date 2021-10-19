fbd80601fc8108a07103995ce85970c480b8ad82
RodrigoRabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2021-10-19 14:30:47 -0300

    logica ci

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,33 @@
 const { src, dest, series, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
+const image = require("gulp-image");
+
+function copyHtml(){
+  return src("./src/*.html").pipe(dest("dist"));
+}
+
+function imgTask(){
+  return src("./src/images/**/*").pipe(image()).pipe(dest("dist/images"))
+}
 
 function buildStyles() {
-  return src("./scss/**/*.scss")
+  return src("./src/scss/**/*.scss")
     .pipe(
       sass({
-        sourceComments: true
+        sourceComments: true,
       })
     )
     .pipe(autoprefixer({ versions: ["last 2 browsers"] }))
-    .pipe(dest("./css"));
+    .pipe(dest("./src/css"));
 }
 
-exports.buildStyles = buildStyles;
-exports.default = ()=> {
-  watch("./scss/**/*.scss", buildStyles)
-};
+function cleanStlyes() {
+  return src("./src/css/**/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("dist"));
+}
 
-// gulp.task("sass", () => {
-//   return gulp.src("./scss/*.scss")
-//     .pipe(sass({
-//       outputStyle: "expanded",
-//       sourceComments: true
-//     }))
-//     .pipe(autoprefixer({
-//       versions: ["last 2 browsers"]
-//     }))
-//     .pipe(gulp.dest("./css"));
-// });
-
-// gulp.task("default", () => {
-//   return gulp.watch("./scss/*.scss", ["sass"]);
-// });
+exports.img = imgTask;
+exports.default = series(copyHtml, imgTask, buildStyles, cleanStlyes,);
