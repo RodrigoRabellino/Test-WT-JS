90a5ac7d585146f58c24a019719a3a6fb4b7f620
RodrigoRabellino <30850683+RodrigoRabellino@users.noreply.github.com>
2021-10-19 15:58:10 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,25 @@ const sass = require("gulp-sass")(require("sass"));
 const cleanCSS = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
 const image = require("gulp-image");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const inject = require("gulp-inject");
 
 function copyHtml(){
   return src("./src/*.html").pipe(dest("dist"));
+}
+
+function injectJs(){
+  let indexjs = src("./dist/js/allScript.js");
+  return src("./dist/index.html").pipe(inject(indexjs)).pipe(dest("./dist"))
+}
+
+function concatJs(){
+  return src("./src/js/*.js").pipe(concat("allScript.js")).pipe(dest("./dist/js"))
+}
+
+function minifyJs(){
+  return src("./src/js/allScript.js").pipe(uglify()).pipe(dest("dist/js"))
 }
 
 function imgTask(){
@@ -30,4 +46,7 @@ function cleanStlyes() {
 }
 
 exports.img = imgTask;
-exports.default = series(copyHtml, imgTask, buildStyles, cleanStlyes,);
+exports.mini = series(concatJs, minifyJs);
+exports.inject = injectJs;
+
+exports.default = series(copyHtml,series(concatJs, minifyJs), injectJs , imgTask, buildStyles, cleanStlyes,);
